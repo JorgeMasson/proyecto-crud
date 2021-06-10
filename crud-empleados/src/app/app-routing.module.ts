3663cf3347e2348c2dd17291cba51d6bb3d994ef
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-06-10 16:40:46 -0700

    Componentes agregados commit

--- crud-empleados/src/app/app-routing.module.ts
+++ crud-empleados/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarComponent } from './COMPONENTES/agregar/agregar.component';
+import { InicioComponent } from './COMPONENTES/inicio/inicio.component';
+import { ModificarComponent } from './COMPONENTES/modificar/modificar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'add', component: AgregarComponent},
+  {path: 'edit', component: ModificarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud-empleados/src/app/app.module.ts
+++ crud-empleados/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './COMPONENTES/inicio/inicio.component';
+import { AgregarComponent } from './COMPONENTES/agregar/agregar.component';
+import { ModificarComponent } from './COMPONENTES/modificar/modificar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AgregarComponent,
+    ModificarComponent
   ],
   imports: [
     BrowserModule,
