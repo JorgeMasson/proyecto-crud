61ed6d5062fb6f8d2e6477ae646d4a7b582661a5
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-06-10 22:21:00 -0700

    Servicios commit

--- crud-empleados/src/app/COMPONENTES/inicio/inicio.component.ts
+++ crud-empleados/src/app/COMPONENTES/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {EmpleadoService} from '../../SERVICIOS/empleado.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private EmpleadoService: EmpleadoService) { }
 
   ngOnInit(): void {
+    this.listarEmpleado()
+  }
+
+  listarEmpleado() {
+    this.EmpleadoService.getEmpleados().subscribe(
+      res=>{
+        console.log(res)
+      },
+      err => console.log(err)
+    );
   }
 
 }
