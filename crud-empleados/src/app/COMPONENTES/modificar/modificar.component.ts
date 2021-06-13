bcd4df5f9f624cc0f02d2f428604bea91dbf6b6f
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-06-12 22:58:34 -0700

    Modificar component commit

--- crud-empleados/src/app/COMPONENTES/modificar/modificar.component.ts
+++ crud-empleados/src/app/COMPONENTES/modificar/modificar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Empleado, EmpleadoService} from '../../SERVICIOS/empleado.service';
+import {Router, ActivatedRoute} from '@angular/router';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-modificar',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificarComponent implements OnInit {
 
-  constructor() { }
+  empleado: Empleado = {
+    id_empleado: '',
+    nombre: '',
+    curso: ''
+  };
+
+  constructor(private EmpleadoService: EmpleadoService, 
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id_entrada = <string>this.activatedRoute.snapshot.params.id;
+    console.log('id de entrada' + id_entrada);
+
+    if(id_entrada) {
+      this.EmpleadoService.getUnEmpleado(id_entrada).subscribe(
+          res=>{
+          this.empleado = res[0];
+          console.log(res);
+        },
+        err=>console.log(err)
+      );
+    }
+  }
+
+  modificar() {
+    this.EmpleadoService.editEmpleado(this.empleado.id_empleado, this.empleado).subscribe(
+      res=>{
+        console.log(res);
+      },
+      err=>console.log(err)
+    );
+    this.router.navigate(['/inicio']);
+  }
+
+  inicio() {
+    this.router.navigate(['/inicio']);
   }
 
 }
